4706ac6ff4bf1277c6c62aeac93547b04d92dd81
Wolmer Martins <wolmer.martins@octadesk.com>
2023-07-27 18:33:17 -0300

    feat: uses provider to use global state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from './components/Header'
+import PersonsProvider from './hooks/provider/PersonsProvider'
+import Home from './pages/Home'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Header />
 
       <div className="App-content">
+        <PersonsProvider>
+          <Home />
+        </PersonsProvider>
       </div>
     </div>
   )
