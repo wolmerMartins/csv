e12ad5c06ada165a9fed805d090605f4b2fa8bde
Wolmer Martins <wolmer.martins@octadesk.com>
2023-07-27 18:46:57 -0300

    fix: corrects typo on error message

--- backend/src/modules/users/services/errors/invalid-attributes-error.ts
+++ backend/src/modules/users/services/errors/invalid-attributes-error.ts
@@ -4,7 +4,7 @@ import { VALID_USER_DTO_ATTRIBUTES } from '../user-service'
 export default class InvalidAttributesError extends ApiError {
   constructor(invalidAttributes: string) {
     super(
-      `The attributes are "${invalidAttributes}" are invalid. Must be one of: "${[...VALID_USER_DTO_ATTRIBUTES].join(', ')}"`,
+      `The CSV headers "${invalidAttributes}" are invalid. Must be one of: "${[...VALID_USER_DTO_ATTRIBUTES].join(', ')}"`,
       'invalid_attributes_error',
       400
     )
